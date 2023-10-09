79ff1a99aee95fe75508fdec7cdbfeaaa54a7e8b
“naandalist” <listiananda.apriliawan@gmail.com>
2023-10-09 21:18:12 +0700

    chore: remove unused page

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className='h1-bold'>Tailwind config here</h1>
-      <h2 className='h2-bold'>Tailwind config here</h2>
-    </div>
-  );
-}
